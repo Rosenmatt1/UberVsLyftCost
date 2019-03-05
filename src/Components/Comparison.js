4f72c9c87d5b6bb4b4bc25d65e20f575f387ab72
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-05 15:04:52 -0700

    Added template data for card

--- src/Components/Comparison.js
+++ src/Components/Comparison.js
@@ -7,9 +7,24 @@ const Comparison = (props) => {
       <div className="row comparison">
         <div className="col-2"></div>
 
-        <div className="col-4">Uber</div>
+        <div className="col-4 uber text-center">
+          <h2>Uber</h2>
+          <p>Est Cost:</p>
+          <p>ETA:</p>
+          <button type="button" class="btn btn-dark">Dark</button>
+        </div>
+
+
+
         <p>VS</p>
-        <div className="col-4">Lyft</div>
+
+
+        <div className="col-4 lyft text-center">
+          <h2>Uber</h2>
+          <p>Est Cost:</p>
+          <p>ETA:</p>
+          <button type="button" class="btn btn-dark">Dark</button>
+        </div>
 
         <div className="col-2"></div>
       </div>
