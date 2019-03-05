e81dfe05165ccb8d89c0b44240b61311c1385cdf
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-05 14:06:35 -0700

    Centered form by adding two wrapper columns

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,19 +3,29 @@ import '../App.css'
 
 const Form = (props) => {
   return (
-    <form>
-      <div className="form-group">
-        <label for="pickUpLocation">From</label>
-        <input type="address" className="form-control" placeholder="Enter Pick Up Location" />
+    <div className="container">
+      <div className="row">
+
+        <div className="col-4"></div>
+        <div className="col-4">
+          <form>
+            <div className="form-group">
+              <label for="pickUpLocation">From</label>
+              <input type="address" className="form-control" placeholder="Enter Pick Up Location" />
+            </div>
+            <div className="form-group">
+              <label for="destination">To</label>
+              <input type="address" className="form-control" placeholder="Password" />
+            </div>
+            <div className="form-group form-check">
+            </div>
+            <button type="submit" className="btn btn-primary">Find Best Deal</button>
+          </form>
+          <div className="col-4"></div>
+        </div>
+
       </div>
-      <div className="form-group">
-        <label for="destination">To</label>
-        <input type="address" className="form-control" placeholder="Password" />
-      </div>
-      <div className="form-group form-check">
-      </div>
-      <button type="submit" className="btn btn-primary">Find Best Deal</button>
-    </form>
+    </div>
   )
 }
 
