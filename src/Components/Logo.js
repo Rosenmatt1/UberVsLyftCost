8c65ab4039b3ef48468f5674d7d66f3d0229d586
TONY ADAMS <truvisory@TONYs-MacBook-Pro.local>
2019-03-06 15:16:00 -0700

    Added Styling

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -5,8 +5,11 @@ import logo from './logoImage.png'
 const Logo = (props) => {
   return (
     <div className="container mt-1 text-center">
-      <div className="row">
+      <div className="row justify-content-center">
         <img className="mx-auto" alt="Uber vs. Lyft" src={logo} />
+        <div className="col-10">
+          <h1 className="highlight">Uber Vs. Lyft Price Comparison</h1>
+        </div>
       </div>
     </div>
   )
