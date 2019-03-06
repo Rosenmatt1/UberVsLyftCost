fc989768372158ad446de7f23c6f906711e7af1c
Ryan Leach <ryanleach@Ryans-MacBook-Pro.local>
2019-03-06 15:40:48 -0700

    test

--- src/App.js
+++ src/App.js
@@ -46,8 +46,7 @@ class App extends Component {
 
   fetchUberTime = async (startLat, startLong, endLat, endLong) => {
     localStorage.setItem('uberjwt', 'aA-_gAKRRkPR_7fIhmMU-3IQGKVAYkMKCrMGq5A1')
-    await fetch(`https://cors-anywhere.herokuapp.com/${uberUrl}v1.2/estimates/time?start_latitude=${startLat}&start_longitude=${startLong}&end_latitude=${endLat}&end_longitude=${endLong}`
-, {
+    await fetch(`https://cors-anywhere.herokuapp.com/${uberUrl}v1.2/estimates/time?start_latitude=${startLat}&start_longitude=${startLong}&end_latitude=${endLat}&end_longitude=${endLong}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
