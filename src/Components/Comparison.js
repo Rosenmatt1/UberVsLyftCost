9db8671afbac7a92c1e11e7dd0085f92f69242be
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-03-08 10:34:44 -0700

    Put event listeners back on book buttons becasue merge erased them

--- src/Components/Comparison.js
+++ src/Components/Comparison.js
@@ -2,59 +2,69 @@ import React from 'react'
 import '../App.css'
 import lyft from './stache_fill2.png'
 import uber from './uber.png'
-import {Spring} from 'react-spring/renderprops';
+import { Spring } from 'react-spring/renderprops';
 
 const Comparison = (props) => {
-  const lyftPrice = props.lyftCost < props.uberPrice 
-                      ? 'lyftDeal'
-                      : props.lyftCost > props.uberPrice
-                        ?'badDealUber'
-                        : ''
-  const lyftTime = props.lyftETA < props.uberTime 
-                    ? 'lyftTime'
-                    : props.lyftETA > props.uberTime
-                      ? 'badTimeUber'
-                      : ''
+  const lyftPrice = props.lyftCost < props.uberPrice
+    ? 'lyftDeal'
+    : props.lyftCost > props.uberPrice
+      ? 'badDealUber'
+      : ''
+  const lyftTime = props.lyftETA < props.uberTime
+    ? 'lyftTime'
+    : props.lyftETA > props.uberTime
+      ? 'badTimeUber'
+      : ''
   const uberPrice = props.uberPrice < props.lyftCost
-                    ? 'uberDeal'
-                    : props.uberPrice > props.lyftCost
-                      ? 'badDealLyft'
-                      : ''
+    ? 'uberDeal'
+    : props.uberPrice > props.lyftCost
+      ? 'badDealLyft'
+      : ''
   const uberTime = props.uberTime < props.lyftETA
-                    ? 'uberTime'
-                    : props.uberTime > props.lyftETA
-                      ? 'badTimeLyft'
-                      : ''                                       
+    ? 'uberTime'
+    : props.uberTime > props.lyftETA
+      ? 'badTimeLyft'
+      : ''
   return (
 
-    
+
 
     <Spring
-      from={{opacity:0, marginBottom: -500}}
-      to={{opacity:1, marginBottom: 0}}
+      from={{ opacity: 0, marginBottom: -500 }}
+      to={{ opacity: 1, marginBottom: 0 }}
     >
-      { butts => (
+      {butts => (
         <div style={butts}>
           <div className="container mt-3">
-      <div className="row comparison justify-content-center">
-        <div className="lyft col-4 text-center mt-3 card mr-3 border-dark">
-          <div>
-          <img className="logo" alt="Lyft" src={lyft} />
+            <div className="row comparison justify-content-center">
+              <div className="lyft col-4 text-center mt-3 card mr-3 border-dark">
+                <div>
+                  <img className="logo" alt="Lyft" src={lyft} />
+                </div>
+                <p className={lyftPrice}>Est Cost: ${props.lyftCost}</p>
+                <p className={lyftTime}>ETA: {props.lyftETA} min</p>
+                <button
+                  onClick={(e) => props.selectedLyftRide(e)}
+                  type="button"
+                  className="btn btn-dark mb-3">
+                  Book
+          </button>
+              </div>
+              <div className="uber col-4 text-center mt-3 card ml-3 border-dark">
+                <div>
+                  <img className="logo" alt="Uber" src={uber} />
+                </div>
+                <p className={uberPrice}>Est Cost: ${props.uberPrice}</p>
+                <p className={uberTime}>ETA: {props.uberTime}min</p>
+                <button
+                  onClick={(e) => props.selectedUberRide(e)}
+                  type="button"
+                  className="btn btn-dark mb-3">
+                  Book
+                </button>
+              </div>
+            </div>
           </div>
-          <p className={lyftPrice}>Est Cost: ${props.lyftCost}</p>
-          <p className={lyftTime}>ETA: {props.lyftETA} min</p>
-          <button type="button" className="btn btn-dark mb-3">Book</button>
-        </div>
-        <div className="uber col-4 text-center mt-3 card ml-3 border-dark">
-          <div>
-          <img className="logo" alt="Uber" src={uber} />
-          </div>
-          <p className={uberPrice}>Est Cost: ${props.uberPrice}</p>
-          <p className={uberTime}>ETA: {props.uberTime}min</p>
-          <button type="button" className="btn btn-dark mb-3">Book</button>
-        </div>
-      </div>
-    </div>
         </div>
       )}
     </Spring>
